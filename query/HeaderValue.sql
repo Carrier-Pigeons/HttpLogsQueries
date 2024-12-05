8d609fbeb8e17e7e266ba2072213d4fb5f18b5ef
MichaelCh-Svg <mich-chemps@tutanota.com>
2024-12-05 16:35:26 +0000

    headerOrder removed from grouping.

--- query/HeaderValue.sql
+++ query/HeaderValue.sql
@@ -3,9 +3,9 @@ SELECT
 count(*) AS valCount, 
 SUM(CASE WHEN NOT proxy THEN 1 ELSE 0 END) AS notProxyCount,
 SUM(CASE WHEN proxy THEN 1 ELSE 0 END) AS proxyCount,
-    request, user_agent, header, headerOrder, headerValue 
+    request, user_agent, header, headerValue 
     FROM header h1 
-    group by request, user_agent, header, headerOrder, headerValue)
+    group by request, user_agent, header, headerValue)
 
 SELECT notProxyCount / valCount AS notProxyPercent, 
 proxyCount / valCount AS proxyPercent,
